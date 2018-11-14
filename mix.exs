bf85b8e4b9afd5a0fc4565bf2a80f9da9c16bc99
Markus Bodner <tuacker@gmail.com>
2018-11-14 13:08:58 +0100

    Set version to 1.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule DiscordNotifier.MixProject do
   def project do
     [
       app: :discord_notifier,
-      version: "0.1.0",
+      version: "1.0.0",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps()
